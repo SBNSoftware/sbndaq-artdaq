4b6d6513d3304982efc7cd553a7ad5506085585c
Antoni Aduszkiewicz <aaduszki@fnal.gov>
2019-08-06 14:16:01 -0500

    replaced logs with TRACE commands that get sent to the GUI MsgViewer

--- sbndaq-artdaq/Generators/Common/BernCRTZMQData_generator.cc
+++ sbndaq-artdaq/Generators/Common/BernCRTZMQData_generator.cc
@@ -12,6 +12,8 @@
 
 #include "BernCRT_TRACE_defines.h"
 
+#include <string>
+
 #include <zmq.h>
 
 #define id "BernCRT"
@@ -20,33 +22,36 @@ sbndaq::BernCRTZMQData::BernCRTZMQData(fhicl::ParameterSet const & ps)
   :
   BernCRTZMQ_GeneratorBase(ps)  
 {
-  TLOG_INFO(id) << "BernCRTZMQData constructor called" << TLOG_ENDL; 
+  TRACE(TR_LOG, "BernCRTZMQData constructor called"); 
  
   zmq_data_pub_port_           = ps_.get<std::string>("zmq_data_pub_port");
   zmq_data_receive_timeout_ms_ = ps_.get<int>("zmq_data_receive_timeout_ms",500);
 
-  TLOG_INFO(id) << "BernCRTZMQData constructor : Calling zmq subscriber with port " << zmq_data_pub_port_.c_str() << TLOG_ENDL;
+  TRACE(TR_LOG, std::string("BernCRTZMQData constructor : Calling zmq subscriber with port ")+ zmq_data_pub_port_);
   
   zmq_context_    = zmq_ctx_new();
 
-  TLOG_INFO(id) << "BernCRTZMQData constructor completed" << TLOG_ENDL;  
+  febctl(GETINFO, 0x52);
+
+  TRACE(TR_LOG, "BernCRTZMQData constructor completed");  
 }
 
 sbndaq::BernCRTZMQData::~BernCRTZMQData()
 {
-  TLOG_INFO(id) << "BernCRTZMQData destructor called" << TLOG_ENDL;  
+  TRACE(TR_LOG, "BernCRTZMQData destructor called");  
   zmq_ctx_destroy(zmq_context_);
 
 
+
   Cleanup();
-  TLOG_INFO(id) << "BernCRTZMQData destructor completed" << TLOG_ENDL;  
+  TRACE(TR_LOG, "BernCRTZMQData destructor completed");  
 }
 
 
 
 void sbndaq::BernCRTZMQData::ConfigureStart()
 {
-  TLOG_INFO(id) << "BernCRTZMQData::ConfigureStart() called" << TLOG_ENDL;  
+  TRACE(TR_LOG, "BernCRTZMQData::ConfigureStart() called");  
 
   zmq_subscriber_ = zmq_socket(zmq_context_,ZMQ_SUB);
 
@@ -54,24 +59,24 @@ void sbndaq::BernCRTZMQData::ConfigureStart()
 
   res = zmq_connect(zmq_subscriber_,zmq_data_pub_port_.c_str());
   if(res!=0)
-    TLOG_ERROR(id) << "BernCRTZMQDataZMQ::ConfigureStart() failed to connect." << TLOG_ENDL;
+    TRACE(TR_ERROR, "BernCRTZMQDataZMQ::ConfigureStart() failed to connect.");
 
   res = zmq_setsockopt(zmq_subscriber_,ZMQ_SUBSCRIBE,NULL,0);
   //res = zmq_setsockopt(zmq_subscriber_,ZMQ_RCVTIMEO,&zmq_data_receive_timeout_ms_,2);
 
   if(res!=0)
-    TLOG_ERROR(id) << "BernCRTZMQDataZMQ::ConfigureStart() socket options failed." << TLOG_ENDL;
+    TRACE(TR_ERROR, "BernCRTZMQDataZMQ::ConfigureStart() socket options failed.");
 
-  TLOG_INFO(id) << "BernCRTZMQData::ConfigureStart() completed" << TLOG_ENDL;  
+  TRACE(TR_LOG, "BernCRTZMQData::ConfigureStart() completed");  
 }
 
 void sbndaq::BernCRTZMQData::ConfigureStop()
 {
-  TLOG_INFO(id) << "BernCRTZMQData::ConfigureStop() called" << TLOG_ENDL;
+  TRACE(TR_LOG, "BernCRTZMQData::ConfigureStop() called");
 
   zmq_close(zmq_subscriber_);
 
-  TLOG_INFO(id) << "BernCRTZMQData::ConfigureStop() completed" << TLOG_ENDL;  
+  TRACE(TR_LOG, "BernCRTZMQData::ConfigureStop() completed");  
 }
 
 int sbndaq::BernCRTZMQData::GetDataSetup()
@@ -97,6 +102,8 @@ void sbndaq::BernCRTZMQData::febctl(feb_command command, uint8_t mac5) {
  *  - Handle return string from the function (we need if with GETINFO)
  *  - Do something during unexpected events (throw exceptions?)
  */
+
+  char trace_message[256];
   
   char command_string[8];
   switch (command) {
@@ -106,20 +113,20 @@ void sbndaq::BernCRTZMQData::febctl(feb_command command, uint8_t mac5) {
     case BIAS_OF : strcpy ( command_string, "BIAS_OF");   break;
     case GETINFO : strcpy ( command_string, "GETINFO");   break;
     default:
-      TLOG_ERROR(id)   << "BernCRTZMQData::febctl Command "<<command<<" unrecognized!" << TLOG_ENDL;
+      TRACE(TR_ERROR, "BernCRTZMQData::febctl Unrecognized command!");
       return;
 
   }
   
   void * context = zmq_ctx_new ();
 
-  char socket[] = "tcp://localhost:5555"; //warning: hardcoded value, to be updated!
+  char socket[] = "tcp://:5555"; //warning: hardcoded value, to be updated!
 
   //  Socket to talk to server
-  TLOG_DEBUG(id)   << "BernCRTZMQData::febctl Connecting to febdrv..." << TLOG_ENDL;
+  TRACE(TR_DEBUG, "BernCRTZMQData::febctl Connecting to febdrv...");
   void *requester = zmq_socket (context, ZMQ_REQ);
   if(zmq_connect (requester, socket) < 0) {
-    TLOG_ERROR(id)   << "BernCRTZMQData::febctl Connection to "<<socket<<" failed!" << TLOG_ENDL;
+    TRACE(TR_ERROR, std::string("BernCRTZMQData::febctl Connection to ") + socket + " failed!");
     return;
     //throw exception? return special code?
   }
@@ -129,13 +136,13 @@ void sbndaq::BernCRTZMQData::febctl(feb_command command, uint8_t mac5) {
   memcpy(zmq_msg_data (&request), command_string,7);
   ((uint8_t*)zmq_msg_data (&request))[7]=0;
   ((uint8_t*)zmq_msg_data (&request))[8]=mac5;
-  TLOG_DEBUG(id) << "BernCRTZMQData::febctl Sending command \"" << command_string << "\" to mac5 = " << (char)mac5 << TLOG_ENDL;
+  TRACE(TR_DEBUG, std::string("BernCRTZMQData::febctl Sending command \"") + command_string + "\" to mac5 = " + (char)mac5);
   zmq_msg_send (&request, requester, 0);
   zmq_msg_close (&request);
   zmq_msg_t reply;
   zmq_msg_init (&reply);
   zmq_msg_recv (&reply, requester, 0);
-  TLOG_DEBUG(id) << "BernCRTZMQData::febctl Received reply: "<< (char*)zmq_msg_data (&reply) << TLOG_ENDL;
+  TRACE(TR_DEBUG, std::string("BernCRTZMQData::febctl Received reply: ") + (char*)zmq_msg_data (&reply));
   zmq_msg_close (&reply);
   zmq_close (requester);
   zmq_ctx_destroy (context);
@@ -195,16 +202,16 @@ void sbndaq::BernCRTZMQData::feb_send_bitstreams(uint8_t mac5) {
   const int sc_length    = ConvertAsciiToBitstream(ps_.get<std::string>("CITIROC_SlowControl_bitStream0"), SlowControl_bitStream);
 
   if(mac5==255) {
-    TLOG_ERROR(id)   << "BernCRTZMQData::feb_send_bitstreams Bitstreams cannot be sent to mac5 = 255!" << TLOG_ENDL;
+    TRACE(TR_ERROR, "BernCRTZMQData::feb_send_bitstreams Bitstreams cannot be sent to mac5 = 255!");
     return;
   }
 
   if(probe_length != PROBE_BITSTREAM_LENGTH) {
-    TLOG_ERROR(id)   << "BernCRTZMQData::feb_send_bitstreams Probe bitstream length incorrect: "<<probe_length <<" (expected "<<PROBE_BITSTREAM_LENGTH<<")"<<TLOG_ENDL;
+    TRACE(TR_ERROR, std::string("BernCRTZMQData::feb_send_bitstreams Probe bitstream length incorrect: ") + std::to_string(probe_length) + " (expected " + std::to_string(PROBE_BITSTREAM_LENGTH) + ")");
     return;
   }
   if(sc_length != SLOW_CONTROL_BITSTREAM_LENGTH) {
-    TLOG_ERROR(id)   << "BernCRTZMQData::feb_send_bitstreams Slow Control bitstream length incorrect: "<< sc_length <<" (expected "<<SLOW_CONTROL_BITSTREAM_LENGTH<<")" << TLOG_ENDL;
+    TRACE(TR_ERROR, std::string("BernCRTZMQData::feb_send_bitstreams Slow Control bitstream length incorrect: ") + std::to_string(sc_length) + " (expected " + std::to_string(SLOW_CONTROL_BITSTREAM_LENGTH)+")");
     return;
   }
 
@@ -213,7 +220,7 @@ void sbndaq::BernCRTZMQData::feb_send_bitstreams(uint8_t mac5) {
   void * context = zmq_ctx_new ();
 
   //  Socket to talk to server
-  TLOG_DEBUG(id)   << "BernCRTZMQData::feb_send_bitstreams Connecting to febdrv..."<< TLOG_ENDL;
+  TRACE(TR_DEBUG, "BernCRTZMQData::feb_send_bitstreams Connecting to febdrv...");
   void *requester = zmq_socket (context, ZMQ_REQ);
   zmq_connect (requester, socket);
 
@@ -224,19 +231,19 @@ void sbndaq::BernCRTZMQData::feb_send_bitstreams(uint8_t mac5) {
 
   uint8_t buffer[MAXPACKLEN];
 
-  TLOG_DEBUG(id)   << "BernCRTZMQData::feb_send_bitstreams Sending command "<<cmd<<"..."<< TLOG_ENDL;
+  TRACE(TR_DEBUG, std::string("BernCRTZMQData::feb_send_bitstreams Sending command ") + cmd + "...");
 
   memcpy(buffer,cmd,9);
   memcpy(buffer+9,SlowControl_bitStream,SLOW_CONTROL_BITSTREAM_LENGTH/8);
   memcpy(buffer+9+SLOW_CONTROL_BITSTREAM_LENGTH/8,Probe_bitStream,PROBE_BITSTREAM_LENGTH/8);
   zmq_send ( requester, buffer, (SLOW_CONTROL_BITSTREAM_LENGTH+PROBE_BITSTREAM_LENGTH)/8+9, 0);
 
-  TLOG_DEBUG(id)   << "BernCRTZMQData::feb_send_bitstreams Waiting for reply..."<< TLOG_ENDL;
+  TRACE(TR_DEBUG, "BernCRTZMQData::feb_send_bitstreams Waiting for reply...");
 
   zmq_msg_t reply;
   zmq_msg_init (&reply);
   zmq_msg_recv (&reply, requester, 0);
-  TLOG_DEBUG(id)   << "BernCRTZMQData::feb_send_bitstreams Received reply: "<<(char*)zmq_msg_data (&reply)<< TLOG_ENDL;
+  TRACE(TR_DEBUG, std::string("BernCRTZMQData::feb_send_bitstreams Received reply: ") + (char*)zmq_msg_data (&reply));
   zmq_msg_close (&reply);
   zmq_close (requester);
   zmq_ctx_destroy (context);
@@ -249,7 +256,7 @@ size_t sbndaq::BernCRTZMQData::GetZMQData()
 {
   std::cout << "Calling GetZMQData" << std::endl;
   
-  TLOG_INFO(id) << "BernCRTZMQData::GetZMQData called" << TLOG_ENDL;
+  TRACE(TR_LOG, "BernCRTZMQData::GetZMQData called");
   
   size_t data_size=0;
   size_t events=0;
@@ -263,7 +270,7 @@ size_t sbndaq::BernCRTZMQData::GetZMQData()
   {
     if ( errno != EAGAIN ) // No data awailable now
     {
-      TLOG_ERROR(id) << "BernCRTZMQData::GetFEBData() called " << errno << " " << strerror(errno) << TLOG_ENDL;
+      TRACE(TR_ERROR, std::string("BernCRTZMQData::GetFEBData() called ") +  std::to_string(errno) + " "  + strerror(errno));
     }
     usleep(1000);
     //return 0;
@@ -271,38 +278,38 @@ size_t sbndaq::BernCRTZMQData::GetZMQData()
     
     if( (wait_count%500) == 0 )
     {
-      TLOG_INFO(id) << "\twait count: " << wait_count << TLOG_ENDL;
+      TRACE(TR_LOG, std::string("\twait count: ") + std::to_string(wait_count));
     }
   }
   
-  TLOG_INFO(id) << "\toutside wait loop: " << wait_count << TLOG_ENDL;
+  TRACE(TR_LOG, std::string("\toutside wait loop: ") + std::to_string(wait_count));
   
   if(zmq_msg_size(&feb_data_msg)>0)
   {
-    TLOG_INFO(id) << "BernCRTZMQData::GetZMQData() about to copy" << TLOG_ENDL;
+    TRACE(TR_LOG, "BernCRTZMQData::GetZMQData() about to copy");
     
     std::copy((uint8_t*)zmq_msg_data(&feb_data_msg),
 	      (uint8_t*)zmq_msg_data(&feb_data_msg)+zmq_msg_size(&feb_data_msg), //last event contains time info
 	      reinterpret_cast<uint8_t*>(&ZMQBufferUPtr[events]));
     
-    TLOG_INFO(id) << "BernCRTZMQData::GetZMQData() copied!" << TLOG_ENDL;
+    TRACE(TR_LOG, "BernCRTZMQData::GetZMQData() copied!");
     
     events += zmq_msg_size(&feb_data_msg)/sizeof(BernCRTZMQEvent);
     data_size += zmq_msg_size(&feb_data_msg);
 
-    TLOG_INFO(id) << "BernCRTZMQData::GetZMQData() copied " << events << " events (" << data_size << " size)" << TLOG_ENDL;
+    TRACE(TR_LOG, std::string("BernCRTZMQData::GetZMQData() copied ") + std::to_string(events) + " events (" + std::to_string(data_size) + " size)");
 
     //check : is this too much data for the buffer?
     if( events>ZMQBufferCapacity_ )
     {
-      TLOG_ERROR(id) << "BernCRTZMQData::GetZMQData : Too many events in ZMQ buffer! " << events << TLOG_ENDL;
+      TRACE(TR_ERROR, std::string("BernCRTZMQData::GetZMQData : Too many events in ZMQ buffer! ") + std::to_string(events));
       throw cet::exception("In BernCRTZMQData::GetZMQData, Too many events in ZMQ buffer!");
     }
   }
 
   zmq_msg_close(&feb_data_msg);
 
-  TLOG_INFO(id) << "BernCRTZMQData::GetZMQData() size returned was " << data_size << TLOG_ENDL;
+  TRACE(TR_LOG, std::string("BernCRTZMQData::GetZMQData() size returned was ") + std::to_string(data_size));
 
   return data_size;
 
