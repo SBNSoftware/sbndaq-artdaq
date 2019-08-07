c7013effc88b5db879152adc40882824e74c124f
Antoni Aduszkiewicz <aaduszki@fnal.gov>
2019-08-07 13:39:49 -0500

    Now febctl should not hang infinitely if febdrv is not listening at the given socket

--- sbndaq-artdaq/Generators/Common/Bern/febctl.cc
+++ sbndaq-artdaq/Generators/Common/Bern/febctl.cc
@@ -30,6 +30,8 @@ int main (int argc, char **argv)
   //  Socket to talk to server
   printf ("Connecting to febdrv...\n");
   void *requester = zmq_socket (context, ZMQ_REQ);
+  const int linger = 0; //time in ms to keep pending messages after closing the socket
+  zmq_setsockopt(requester, ZMQ_LINGER, &linger, sizeof(linger));
   rv=zmq_connect (requester, argv[1]); 
   if(rv<0) 
   { printf("Connection to %s failed!\n",argv[1]); return -1;}
@@ -42,15 +44,22 @@ int main (int argc, char **argv)
   memcpy(cmd, argv[2],7); cmd[7]=0;
   ((uint8_t*)zmq_msg_data (&request))[7]=0;
   ((uint8_t*)zmq_msg_data (&request))[8]=mac5;
-  printf ("Sending command %s...", cmd);
+  printf ("Sending command %s...\n", cmd);
   zmq_msg_send (&request, requester, 0);
   zmq_msg_close (&request);
   zmq_msg_t reply;
+  const int timeout = 5*1000*1000; //µs
   zmq_msg_init (&reply);
-  zmq_msg_recv (&reply, requester, 0);
-  printf ("Received reply: %s\n", (char*)zmq_msg_data (&reply));
+  rv = zmq_msg_recv (&reply, requester, ZMQ_DONTWAIT);
+  for(int i = 0; i < 10 && rv < 0; i++) {
+    printf("\twaiting for answer...\n");
+    usleep(timeout/10);
+    rv = zmq_msg_recv (&reply, requester, ZMQ_DONTWAIT);
+  }
+  if(rv>=0) printf ("Received reply: %s\n", (char*)zmq_msg_data (&reply));
+  else printf ("Error: received no reply after waiting for %d seconds\n", timeout / 1000000);
   zmq_msg_close (&reply);
   zmq_close (requester);
-  zmq_ctx_destroy (context);
+  zmq_ctx_destroy (context); 
   return 0;
 }
