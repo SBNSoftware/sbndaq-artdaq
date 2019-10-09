15fb5599abf996a7e59223dcaae7c8db76c2fdee
Antoni Aduszkiewicz <aaduszki@fnal.gov>
2019-10-09 14:39:57 -0500

    Save metadata

--- sbndaq-artdaq/ArtModules/Common/BernCRTZMQAna_module.cc
+++ sbndaq-artdaq/ArtModules/Common/BernCRTZMQAna_module.cc
@@ -51,15 +51,34 @@ private:
 
   TTree * events;
 
-   uint8_t mac5;
+   uint8_t mac5; //last 8 bits of FEB mac5 address
    uint16_t flags;
    uint16_t lostcpu;
    uint16_t lostfpga;
    uint32_t ts0;
    uint32_t ts1;
-   //const uint16_t * adc;
    uint16_t adc[32];
    uint32_t coinc;
+
+//metadata
+//Saving times as double causes loss of precision from 1 ns to some 200 ns, but for poll timestamps that should be still sufficient
+   double    run_start_time;
+   double    time_poll_start;
+   double    time_poll_finish;
+   double    time_last_poll_start;
+   double    time_last_poll_finish;
+   double    fragment_fill_time;
+   uint32_t  feb_event_count;
+   uint32_t  gps_counter;
+   uint32_t  events_in_data_packet;
+   uint32_t  sequence_number;
+
+   uint32_t missed_events;
+   uint32_t overwritten_events;
+   uint32_t dropped_events;
+   uint32_t n_events;
+   uint32_t n_datagrams;
+
   
 };
 
@@ -80,14 +99,33 @@ sbndaq::BernCRTZMQAna::BernCRTZMQAna(fhicl::ParameterSet const & pset)
 
   events = tfs->make<TTree>("events", "FEB events");
 
-  events->Branch("mac5",        &mac5,  "mac5/b");
-  events->Branch("flags",       &flags, "flags/s");
+//event data
+  events->Branch("mac5",        &mac5,          "mac5/b");
+  events->Branch("flags",       &flags,         "flags/s");
   events->Branch("lostcpu",     &lostcpu,       "lostcpu/s");
   events->Branch("lostfpga",    &lostcpu,       "lostfpga/s");
   events->Branch("ts0",         &ts0,           "ts0/i");
   events->Branch("ts1",         &ts1,           "ts1/i");
   events->Branch("adc",         &adc,           "adc[32]/s");
   events->Branch("coinc",       &coinc,         "coinc/i");
+
+//metadata
+  events->Branch("run_start_time",            &run_start_time,              "run_start_time/d");
+  events->Branch("time_poll_start",           &time_poll_start,             "time_poll_start/d");
+  events->Branch("time_poll_finish",          &time_poll_finish,            "time_poll_finish/d");
+  events->Branch("time_last_poll_start",      &time_last_poll_start,        "time_last_poll_start/d");
+  events->Branch("time_last_poll_finish",     &time_last_poll_finish,       "time_last_poll_finish/d");
+  events->Branch("fragment_fill_time",        &fragment_fill_time,          "fragment_fill_time/d");
+  events->Branch("feb_event_count",           &feb_event_count,             "feb_event_count/i");
+  events->Branch("gps_counter",               &gps_counter,                 "gps_counter/i");
+  events->Branch("events_in_data_packet",     &events_in_data_packet,       "events_in_data_packet/i");
+  events->Branch("sequence_number",           &sequence_number,             "sequence_number/i");
+
+  events->Branch("missed_events",             &missed_events,               "missed_events/i");
+  events->Branch("overwritten_events",        &overwritten_events,          "overwritten_events/i");
+  events->Branch("dropped_events",            &dropped_events,              "dropped_events/i");
+  events->Branch("n_events",                  &n_events,                    "n_events/i");
+  events->Branch("n_datagrams",               &n_datagrams,                 "n_datagrams/i");
 
 }
 
@@ -129,49 +167,63 @@ void sbndaq::BernCRTZMQAna::analyze(art::Event const & evt)
     const auto& frag((*rawFragHandle)[idx]); // use this fragment as a reference to the same data
 
     //this applies the 'overlay' to the fragment, to tell it to treat it like a BernCRTZMQ fragment
-    BernCRTZMQFragment bb(frag);
+    BernCRTZMQFragment bern_fragment(frag);
     
-    //gets a pointer to the metadata from the fragment
-    //type is BernCRTZMQFragmentMetadata*
-    auto const* md = bb.metadata();
+    const BernCRTZMQFragmentMetadata* md = bern_fragment.metadata();
     std::cout << *md << std::endl;
 
     //gets the number of CRT events (hits) inside this fragment
     size_t n_events = md->n_events();
 
     //loop over the events in the fragment ...
-    for(size_t i_e=0; i_e<n_events; ++i_e){
+    for(size_t i_e=0; i_e<n_events; ++i_e) {
 
       //grab the pointer to the event
-      BernCRTZMQEvent const* evt = bb.eventdata(i_e);
+      BernCRTZMQEvent const* bevt = bern_fragment.eventdata(i_e);
       
       //we can print this
-      std::cout << *evt << std::endl;
+      std::cout << *bevt << std::endl;
 
       //write all timestamps in an outuputfile
-      //outputFile << evt->Time_TS0() << std::endl;
+      //outputFile << bevt->Time_TS0() << std::endl;
 
 
       //let's fill our sample hist with the Time_TS0()-1e9 if 
       //it's a GPS reference pulse
-      if(evt->IsReference_TS0()){
-	fSampleHist->Fill(evt->Time_TS0()-1e9);
-        //fSampleHist_TS0 -> Fill(evt->Time_TS0());
-       
+      if(bevt->IsReference_TS0()){
+	fSampleHist->Fill(bevt->Time_TS0()-1e9);
       }
 
-      TS0 -> Fill(evt->Time_TS0());
+      TS0 -> Fill(bevt->Time_TS0());
 
-      mac5 = evt->MAC5();
-      flags = evt->flags;
-      lostcpu = evt->lostcpu;
-      lostfpga = evt->lostfpga;
-      ts0 = evt->Time_TS0();
-      ts1 = evt->Time_TS1();
-      coinc = evt->coinc;
+//event data
+      mac5     = bevt->MAC5();
+      flags    = bevt->flags;
+      lostcpu  = bevt->lostcpu;
+      lostfpga = bevt->lostfpga;
+      ts0      = bevt->Time_TS0();
+      ts1      = bevt->Time_TS1();
+      coinc    = bevt->coinc;
 
-      //adc = evt->ADC();
-      for(int ch=0; ch<32; ch++) adc[ch] = evt->ADC(ch);
+      for(int ch=0; ch<32; ch++) adc[ch] = bevt->ADC(ch);
+
+//metadata
+      run_start_time            = md->run_start_time_seconds()        + 1e-9 * md->run_start_time_nanosec();
+      time_poll_start           = md->time_poll_start_seconds()       + 1e-9 * md->time_poll_start_nanosec();
+      time_poll_finish          = md->time_poll_finish_seconds()      + 1e-9 * md->time_poll_finish_nanosec();
+      time_last_poll_start      = md->time_last_poll_start_seconds()  + 1e-9 * md->time_last_poll_start_nanosec();
+      time_last_poll_finish     = md->time_last_poll_finish_seconds() + 1e-9 * md->time_last_poll_finish_nanosec();
+      fragment_fill_time        = md->fragment_fill_time_seconds()    + 1e-9 * md->fragment_fill_time_nanosec();
+      feb_event_count           = md->feb_event_count();
+      gps_counter               = md->gps_count();
+      events_in_data_packet     = md->event_packet();
+      sequence_number           = md->sequence_number();
+
+      missed_events             = md->missed_events();
+      overwritten_events        = md->overwritten_events();
+      dropped_events            = md->dropped_events();
+      n_events                  = md->n_events();
+      n_datagrams               = md->n_datagrams();
 
       events->Fill();
 
