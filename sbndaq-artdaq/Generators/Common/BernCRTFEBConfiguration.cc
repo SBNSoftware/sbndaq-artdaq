744cfc527122a0aea7419875d4b767f55c84eb98
Antoni Aduszkiewicz <antoni.aduszkiewicz@cern.ch>
2020-02-27 19:18:09 -0600

    Human readable configuration for CRT - code to read it and store it

--- sbndaq-artdaq/Generators/Common/BernCRTFEBConfiguration.cc
+++ sbndaq-artdaq/Generators/Common/BernCRTFEBConfiguration.cc
@@ -10,23 +10,23 @@ sbndaq::BernCRTFEBConfiguration::BernCRTFEBConfiguration() {
 
 
 sbndaq::BernCRTFEBConfiguration::BernCRTFEBConfiguration(fhicl::ParameterSet const & ps_, int iFEB) {
-  /**
-   * Constructor basing on FHiCL file
-   */
-  if(!ASCIIToBitStream(
-        ps_.get<std::string>("ProbeBitStream"),
-        ProbeBitStream,
-        PROBE_BITSTREAM_NBITS)) {   
-    TLOG(TLVL_ERROR)<<__func__ << " Failed to load PROBE bit stream";
-    throw cet::exception(std::string(TRACE_NAME) + "::" + __func__ + " Failed to load PROBE bit stream");
+  parameters_initialized = false;
+  
+//Configuration in the FHiCL file can have two formats, and the format is detected automatically
+//if bitstream exists in FHiCL file, it is loaded, otherwise human readable format is used
+  std::string s = ps_.get<std::string>("SlowControlBitStream"+std::to_string(iFEB), "failure");
+  if(s.compare("failure")) {
+    TLOG(TLVL_INFO)<< __func__ << " Loading human-readable configuration for FEB" + iFEB;
+    if(!load_readable_format(ps_, iFEB))
+      throw cet::exception(std::string(TRACE_NAME) + "::" + __func__ + " Failed to load FHiCL configuration for FEB " + std::to_string(iFEB));
   }
-  if(!ASCIIToBitStream(
-        ps_.get<std::string>("SlowControlBitStream"+std::to_string(iFEB)),
-        SlowControlBitStream,
-        SLOW_CONTROL_BITSTREAM_NBITS)) {   
-    TLOG(TLVL_ERROR)<< __func__  << " Failed to load Slow Control bit stream";
-    throw cet::exception(std::string(TRACE_NAME) + "::" + __func__ + " Failed to load Slow Control bit stream");
+  else {
+    TLOG(TLVL_INFO)<< __func__ << " Loading bitstream configuration for FEB" + iFEB;
+    if(!load_bitstream_format(ps_, iFEB))
+      throw cet::exception(std::string(TRACE_NAME) + "::" + __func__ + " Failed to load FHiCL configuration for FEB " + std::to_string(iFEB));
   }
+  
+  //load HV on/off settings and cable delays
   std::vector<bool> hv_on_permissions = ps_.get< std::vector<bool> >("TurnOnHV");
   std::vector<int32_t> PPS_offsets = ps_.get< std::vector<int32_t> >("PPS_offset_ns");
   std::vector<uint64_t> mac5s = ps_.get< std::vector<uint64_t> >("MAC5s"); //read MAC5 list for validation purposes only
@@ -43,6 +43,237 @@ sbndaq::BernCRTFEBConfiguration::BernCRTFEBConfiguration(fhicl::ParameterSet con
     throw cet::exception(std::string(TRACE_NAME) + "::" + __func__ + " PPS_offset_ns array size differs from MAC5s array size");
   }
   PPS_offset = PPS_offsets[iFEB];
+}
+
+bool sbndaq::BernCRTFEBConfiguration::load_bitstream_format(fhicl::ParameterSet const & ps_, int iFEB) {
+  /**
+   * Constructor basing on FHiCL file
+   */
+  if(!ASCIIToBitStream(
+        ps_.get<std::string>("ProbeBitStream"),
+        ProbeBitStream,
+        PROBE_BITSTREAM_NBITS)) {   
+    TLOG(TLVL_ERROR)<<__func__ << " Failed to load PROBE bit stream";
+    return false;
+    
+  }
+  if(!ASCIIToBitStream(
+        ps_.get<std::string>("SlowControlBitStream"+std::to_string(iFEB)),
+        SlowControlBitStream,
+        SLOW_CONTROL_BITSTREAM_NBITS)) {   
+    TLOG(TLVL_ERROR)<< __func__  << " Failed to load Slow Control bit stream";
+    return false;
+    throw cet::exception(std::string(TRACE_NAME) + "::" + __func__ + " Failed to load Slow Control bit stream");
+  }
+  return true;
+}
+
+
+std::vector<std::string> sbndaq::BernCRTFEBConfiguration::channel_parameter_names = {
+  /**
+   * This vector is needed to know the order of the channel parameters in the FHiCL array
+   */
+  "time_threshold_adjustment",
+  "charge_threshold_adjustment",
+  "activate_discriminator",
+  "hv",
+  "high_high_bias",
+  "HG_gain",
+  "LG_gain",
+  "test_HG",
+  "test_LG",
+  "enable_preamp"
+};
+
+void sbndaq::BernCRTFEBConfiguration::InitializeParameters() {
+  /**
+   * This function holds description of all parameters
+   * It allows to manipulate them easily, read from FHiCL file and convert to
+   * bitstream or vice versa.
+   * 
+   * It initializes four maps:
+   *  · boolean_parameters
+   *  · numerical_parameters
+   *  · boolean_channel_parameters
+   *  · numerical_channel_parameters
+   * The first two hold information of parameters that appear only once for an FEB.
+   * As boolean parameters require only 1 bit, and have values of false or true,
+   * they use simpler structure.
+   * The last two maps hold information of parameters for each of the 32 channels.
+   * Their names are in format like "ch13_time_threshold_adjustment".
+   * 
+   * Each entry initializes dummy value for the parameter (0 or false), as well as
+   * location in the bitstream, a comment (as needed) and, for the numerical values,
+   * maximum allowed value
+   */
+  
+  if(parameters_initialized) return;
+  else parameters_initialized = true;
+  
+  for(int channel = 0; channel < 32; channel++) {
+    std::string ch = "channel"+std::to_string(channel)+"_";
+    numerical_channel_parameters[ch+"time_threshold_adjustment"]   = {0, 15, uint16_t(  0 + channel* 4), uint16_t(  3 + channel* 4), ""};  
+    numerical_channel_parameters[ch+"charge_threshold_adjustment"] = {0, 15, uint16_t(128 + channel* 4), uint16_t(131 + channel* 4), ""};  
+    numerical_channel_parameters[ch+"hv"]                          = {0,255, uint16_t(331 + channel* 9), uint16_t(338 + channel* 9), ""};  
+    numerical_channel_parameters[ch+"HG_gain"]                     = {0, 31, uint16_t(619 + channel*15), uint16_t(624 + channel*15), ""};  
+    numerical_channel_parameters[ch+"LG_gain"]                     = {0, 31, uint16_t(625 + channel*15), uint16_t(630 + channel*15), ""};
+    boolean_channel_parameters  [ch+"activate_discriminator"] = {false, uint16_t(265 + channel* 1), false, ""};
+    boolean_channel_parameters  [ch+"high_high_bias"]         = {false, uint16_t(339 + channel* 9), false, ""};
+    boolean_channel_parameters  [ch+"test_HG"]                = {false, uint16_t(631 + channel*15), false, ""};
+    boolean_channel_parameters  [ch+"test_LG"]                = {false, uint16_t(632 + channel*15), false, ""};
+    boolean_channel_parameters  [ch+"enable_preamp"]          = {false, uint16_t(633 + channel*15), false, ""};
+  }
+
+//                                                         value, bit, inverted_logic, comment
+  boolean_parameters["enable_charge_discriminator"]     = {false, 256, false, ""};
+  boolean_parameters["enable_charge_discriminator_PP"]  = {false, 257, true,  ""};
+  boolean_parameters["latched_output"]                  = {false, 258, false, "True: latched output, False: direct output"};
+
+  boolean_parameters["enable_time_discriminator"]       = {false, 259, false, ""};
+  boolean_parameters["enable_time_discriminator_PP"]    = {false, 260, true,  ""};
+
+  boolean_parameters["enable_4bit_charge_DAC"]          = {false, 261, false, ""};
+  boolean_parameters["enable_4bit_charge_DAC_PP"]       = {false, 262, false, ""};
+  boolean_parameters["enable_4bit_time_DAC"]            = {false, 263, false, ""};
+  boolean_parameters["enable_4bit_time_DAC_PP"]         = {false, 264, false, ""};
+
+  boolean_parameters["enable_HG_SCA_PP"]                = {false, 297, false, ""};
+  boolean_parameters["enable_HG_SCA"]                   = {false, 298, true,  ""};
+  boolean_parameters["enable_LG_SCA_PP"]                = {false, 299, false, ""};
+  boolean_parameters["enable_LG_SCA"]                   = {false, 300, true,  ""};
+
+  boolean_parameters["SCA_high_bias"]                   = {false, 301, false, "True: high bias 5MHz readout speed, False: weak bias"};
+
+  boolean_parameters["HG_peak_detector_PP"]             = {false, 302, false, "True: power pin, False: forced"};
+  boolean_parameters["enable_HG_peak_detector"]         = {false, 303, true,  ""};
+
+  boolean_parameters["LG_peak_detector_PP"]             = {false, 304, false, "True: power pin, False: forced"};
+  boolean_parameters["enable_LG_peak_detector"]         = {false, 305, true,  ""};
+
+  boolean_parameters["select_HG_mode"]                  = {false, 306, false, "True: track & hold (SCA), False: peak detector"};
+  boolean_parameters["select_LG_mode"]                  = {false, 307, false, ""};
+
+  boolean_parameters["bypass_peak_sensing_cell"]        = {false, 308, false, ""};
+  boolean_parameters["peak_sensing_cell"]               = {false, 309, false, "True: external trigger, False: internal trigger"};
+
+  boolean_parameters["enable_fast_shaper_follower_PP"]  = {false, 310, true,  ""};
+  boolean_parameters["enable_fast_shaper"]              = {false, 311, false, ""};
+  boolean_parameters["enable_fast_shaper_PP"]           = {false, 312, true,  ""};
+
+  boolean_parameters["enable_LG_slow_shaper_PP"]        = {false, 313, true,  ""};
+  boolean_parameters["enable_LG_slow_shaper"]           = {false, 314, false, ""};
+//                                                        value, max_value, first_bit last_bit, comment
+  numerical_parameters["LG_shaper_time_constant"]       = {0, 7, 315, 317, "0: 87.5ns, 1: 75.0ns, 2: 62.5ns, 3: 50.0ns, 4: 37.5ns, 5: 25.0ns, 6: 12.5ns, 7: undefined?"};
+
+  boolean_parameters["enable_HG_slow_shaper_PP"]        = {false, 318, true, ""};
+  boolean_parameters["enable_HG_slow_shaper"]           = {false, 319, false, ""};
+  numerical_parameters["HG_shaper_time_constant"]       = {0, 7, 320, 322, "0: 87.5ns, 1: 75.0ns, 2: 62.5ns, 3: 50.0ns, 4: 37.5ns, 5: 25.0ns, 6: 12.5ns, 7: undefined?"};
+
+  boolean_parameters["LG_preamp_bias"]                  = {false, 323, true,  "True: normal, False: weak"};
+
+  boolean_parameters["enable_HG_preamp_PP"]             = {false, 324, true,  ""};
+  boolean_parameters["enable_HG_preamp"]                = {false, 325, false, ""};
+
+  boolean_parameters["enable_LG_preamp_PP"]             = {false, 326, true,  ""};
+  boolean_parameters["enable_LG_preamp"]                = {false, 327, false, ""};
+
+  boolean_parameters["preamp_connected_to_fast_shaper"] = {false, 328, false, "True: HG, False: LG"};
+
+  boolean_parameters["enable_32_input_8bit_DAC"]        = {false, 329, false, ""};
+  boolean_parameters["input_DAC_voltage_reference"]     = {false, 330, false, "True: external (?) 4.5V, False: internal 2.5V"};
+
+  boolean_parameters["enable_temperature_sensor_PP"]    = {false, 1099, true,  ""};
+  boolean_parameters["enable_temperature_sensor"]       = {false, 1100, false, ""};
+
+  boolean_parameters["enable_BandGap_sensor_PP"]        = {false, 1101, true,  ""};
+  boolean_parameters["enable_BandGap_sensor"]           = {false, 1102, false, ""};
+
+  boolean_parameters["enable_charge_DAC"]               = {false, 1103, false, ""};
+  boolean_parameters["enable_charge_DAC_PP"]            = {false, 1104, true,  ""};
+
+  boolean_parameters["enable_time_DAC"]                 = {false, 1105, false, ""};
+  boolean_parameters["enable_time_DAC_PP"]              = {false, 1106, true,  ""};
+
+  numerical_parameters["charge_threshold"]              = {0, 1023, 1107, 1116, "DAC1"};
+  numerical_parameters["time_threshold"]                = {0, 1023, 1117, 1126, "DAC2"};
+
+  boolean_parameters["enable_HG_OTA"]                   = {false, 1127, false, ""};
+  boolean_parameters["enable_HG_OTA_PP"]                = {false, 1128, true,  ""};
+
+  boolean_parameters["enable_LG_OTA"]                   = {false, 1129, false, ""};
+  boolean_parameters["enable_LG_OTA_PP"]                = {false, 1130, true,  ""};
+
+  boolean_parameters["enable_probe_OTA"]                = {false, 1131, false, ""};
+  boolean_parameters["enable_probe_OTA_PP"]             = {false, 1132, true,  ""};
+
+  boolean_parameters["enable_OTAQ_test_bit"]            = {false, 1133, false, ""};
+
+  boolean_parameters["enable_VAL_EVT_OTA"]              = {false, 1134, false, ""};
+  boolean_parameters["enable_VAL_EVT_OTA_PP"]           = {false, 1135, true,  ""};
+
+  boolean_parameters["enable_RAZ_CHN_OTA"]              = {false, 1136, false, ""};
+  boolean_parameters["enable_RAZ_CHN_OTA_PP"]           = {false, 1137, true,  ""};
+
+  boolean_parameters["enable_digital_multiplexed_output"] = {false, 1138, false, ""};
+  boolean_parameters["enable_OR32_output"]                = {false, 1139, false, ""};
+  boolean_parameters["enable_OR32_open_collector_output"] = {false, 1140, false, ""};
+
+  boolean_parameters["trigger_polarity"]                  = {false, 1141, false, "True: positive (rising edge), False: negative (falling edge)"};
+
+  boolean_parameters["enable_digital_OR32_T_open_collector_output"] = {false, 1142, false, ""};
+  boolean_parameters["enable_32_channels_triggers_output"]          = {false, 1143, false, ""};
+  
+}
+
+bool sbndaq::BernCRTFEBConfiguration::load_readable_format(fhicl::ParameterSet const & ps_, int MAC5) {
+  InitializeParameters();
+  
+  //read global parameters
+  for(auto parameter : boolean_parameters) {
+    boolean_parameters[parameter.first].value = ps_.get<bool>(parameter.first);
+  }
+  for(auto parameter : numerical_parameters) {
+    numerical_parameters[parameter.first].value = ps_.get<uint16_t>(parameter.first);
+    if(numerical_parameters[parameter.first].value > numerical_parameters[parameter.first].max_value) {
+      TLOG(TLVL_ERROR)<<__func__ << " Failed to load "<<parameter.first<<" for MAC "<<MAC5<<": "<<numerical_parameters[parameter.first].value<<" > "<<numerical_parameters[parameter.first].max_value;
+      return false;
+    }
+  }
+  
+  //read individual channel parameters
+  //they are stored in an array, which we will split into individual parameters
+  std::vector< std::vector<uint16_t> > fhicl_channel_array  = ps_.get< std::vector< std::vector<uint16_t> > >("channel_configuration");
+  
+  if(fhicl_channel_array.size() != 32) {
+    TLOG(TLVL_ERROR)<<__func__ << " Invalid size of channel configuration for MAC "<<MAC5<<": "<<fhicl_channel_array.size()<<" ≠ 32";
+    return false;
+  }
+  for(int channel = 0; channel < 32; channel++) {
+    if(fhicl_channel_array[channel].size() != 10) {
+      TLOG(TLVL_ERROR)<<__func__ << " Invalid size of channel "<<channel<<" configuration for MAC "<<MAC5<<": "<<fhicl_channel_array[channel].size()<<" ≠ 10";
+      return false;
+    }
+    
+    for(unsigned int parameter = 0; parameter < channel_parameter_names.size(); parameter++) {
+      std::string name = "channel"+std::to_string(channel)+"_"+channel_parameter_names[parameter];
+      //check if it is boolean parameter or numerical parameter
+      if(boolean_channel_parameters.count(name)) {
+        boolean_channel_parameters[name].value = fhicl_channel_array[channel][parameter];
+      }
+      else if(numerical_channel_parameters.count(name)) {
+        if(fhicl_channel_array[channel][parameter] > numerical_channel_parameters[name].max_value) {
+          TLOG(TLVL_ERROR)<<__func__ << " MAC "<<MAC5<<", channel "<<channel<<", parameter "<<parameter<<" ("<<name<<") value "<<fhicl_channel_array[channel][parameter]<<" > "<<numerical_channel_parameters[name].max_value;
+          return false;
+        }
+        numerical_channel_parameters[name].value = fhicl_channel_array[channel][parameter];
+      }
+      else { //oopsie
+        TLOG(TLVL_ERROR)<<__func__ << " Parameter "<<parameter<<" not found, internal error, blame the coder";
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 int sbndaq::BernCRTFEBConfiguration::ASCIIToBitStream(std::string ASCIIBitStream, uint8_t *bitstream, unsigned int nBits) {
--- sbndaq-artdaq/Generators/Common/BernCRTFEBConfiguration.hh
+++ sbndaq-artdaq/Generators/Common/BernCRTFEBConfiguration.hh
@@ -15,10 +15,10 @@ namespace sbndaq {
 
   class BernCRTFEBConfiguration  {
     public:
-
+      
       BernCRTFEBConfiguration(fhicl::ParameterSet const & ps_, int iFEB);
       BernCRTFEBConfiguration();
-
+      
       uint8_t * GetProbeBitStream();
       int       GetProbeBitStreamNBytes();
       uint8_t * GetSlowControlBitStream();
@@ -26,16 +26,19 @@ namespace sbndaq {
       std::string GetString(std::string separator=" # ");
       bool      GetHVOnPermission();
       int32_t   GetPPSOffset();
-
+      
     private:
       static const int PROBE_BITSTREAM_NBITS = 224;
       static const int SLOW_CONTROL_BITSTREAM_NBITS = 1144;
       uint8_t ProbeBitStream[PROBE_BITSTREAM_NBITS/8];
       uint8_t SlowControlBitStream[SLOW_CONTROL_BITSTREAM_NBITS/8];
       bool hv_on_permission;
-      int32_t PPS_offset;
+      int32_t PPS_offset; //cable delay in ns
 
-
+      //constructors
+      bool load_bitstream_format(fhicl::ParameterSet const & ps_, int iFEB);
+      bool load_readable_format(fhicl::ParameterSet const & ps_, int MAC5);
+      
       static int GetBit(unsigned int bit_number, uint8_t * bitstream, unsigned int nBits);
       int GetBit(unsigned int bit_number);
       static void SetBit(unsigned int bit_number, bool value, uint8_t * bitstream, unsigned int nBits);
@@ -44,6 +47,32 @@ namespace sbndaq {
       std::string BitsToASCII(unsigned int bit_number);
 
       static int ASCIIToBitStream(std::string ASCIIBitStream, uint8_t * bitstream, unsigned int nBits);
+      
+      //Human readable configuration
+      struct boolean_parameter {
+        bool        value;
+        uint16_t    bit;
+        bool        inverted_logic; //if "true" corresponds to bit "0" in bit stream
+        std::string comment;
+      };
+      
+      struct numerical_parameter {
+        uint16_t    value;
+        uint16_t    max_value;
+        uint16_t    first_bit;
+        uint16_t    last_bit;
+        std::string comment;
+      };
+      
+      std::map <std::string, boolean_parameter> boolean_parameters;
+      std::map <std::string, numerical_parameter> numerical_parameters;
+      std::map <std::string, boolean_parameter> boolean_channel_parameters;
+      std::map <std::string, numerical_parameter> numerical_channel_parameters;
+      
+      static std::vector<std::string> channel_parameter_names;
+      
+      void InitializeParameters();
+      bool parameters_initialized;
   };
 }
 
