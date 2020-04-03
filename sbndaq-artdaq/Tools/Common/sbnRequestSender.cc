81f1ae8787dcd1273cda1a8183e1f2a24919ed30
Antoni Aduszkiewicz <antoni.aduszkiewicz@cern.ch>
2020-04-02 21:38:22 -0500

    Send requests in steady time periods;example configuration

--- sbndaq-artdaq/Tools/Common/sbnRequestSender.cc
+++ sbndaq-artdaq/Tools/Common/sbnRequestSender.cc
@@ -15,18 +15,16 @@ int main(int argc, char* argv[]) {
   struct Config {
     fhicl::TableFragment<artdaq::RequestSender::Config> senderConfig;
     fhicl::Atom<bool> use_receiver{fhicl::Name{"use_receiver"}, fhicl::Comment{"Whether to setup a RequestReceiver to verify that requests are being sent"}, false};
-    fhicl::Atom<size_t> receiver_timeout_ms{fhicl::Name{"recevier_timeout_ms"}, fhicl::Comment{"Amount of time to wait for the receiver to receive a request message"}, 1000};
+    fhicl::Atom<size_t> receiver_timeout_ms{fhicl::Name{"receiver_timeout_ms"}, fhicl::Comment{"Amount of time to wait for the receiver to receive a request message"}, 1000};
     fhicl::Table<artdaq::RequestReceiver::Config> receiver_config{fhicl::Name{"receiver_config"}, fhicl::Comment{"Configuration for RequestReceiver, if used"}};
     fhicl::Atom<int> num_requests{fhicl::Name{"num_requests"}, fhicl::Comment{"Number of requests to send"}};
     fhicl::Atom<artdaq::Fragment::sequence_id_t> starting_sequence_id{fhicl::Name{"starting_sequence_id"}, fhicl::Comment{"Sequence ID of first request"}, 1};
-    fhicl::Atom<artdaq::Fragment::sequence_id_t> sequence_id_scale{fhicl::Name{"sequence_id_scale"}, fhicl::Comment{"Amount to increment Sequence ID for each request"}, 1};
-    fhicl::Atom<artdaq::Fragment::timestamp_t> starting_timestamp{fhicl::Name{"starting_timestamp"}, fhicl::Comment{"Timestamp of first request"}, 1};
-    fhicl::Atom<artdaq::Fragment::timestamp_t> timestamp_scale{fhicl::Name{"timestamp_scale"}, fhicl::Comment{"Amount to increment timestamp for each request"}, 1};
+    fhicl::Atom<artdaq::Fragment::sequence_id_t> sequence_id_scale{fhicl::Name{"sequence_id_step"}, fhicl::Comment{"Amount to increment Sequence ID for each request"}, 1};
+    fhicl::Atom<artdaq::Fragment::timestamp_t> timestamp_scale{fhicl::Name{"time_step_ms"}, fhicl::Comment{"Number of milliseconds between requests"}, 211};
   };
 
   auto pset = LoadParameterSet<Config>(argc, argv, "sender", "This test application sends Data Request messages and optionally receives them to detect issues in the network transport");
 
-  int rc = 0;
 
   artdaq::RequestSender sender(pset);
 
@@ -38,13 +36,20 @@ int main(int argc, char* argv[]) {
   }
 
   auto seq = pset.get<artdaq::Fragment::sequence_id_t>("starting_sequence_id", 1);
-  auto seq_scale = pset.get<artdaq::Fragment::sequence_id_t>("sequence_id_scale", 1);
-  auto ts = pset.get<artdaq::Fragment::timestamp_t>("starting_timestamp", 1);
-  auto ts_scale = pset.get<artdaq::Fragment::timestamp_t>("timestamp_scale", 1);
+  auto seq_scale = pset.get<artdaq::Fragment::sequence_id_t>("sequence_id_step", 1);
   auto tmo = pset.get<size_t>("recevier_timeout_ms", 1000);
 
-  for (auto ii = 0; ii < num_requests; ++ii) {
-    sender.AddRequest(seq, ts);
+  std::chrono::time_point<std::chrono::system_clock> request_time = std::chrono::system_clock::now();
+  std::chrono::duration<int, std::ratio<1, 1000>> time_step = std::chrono::milliseconds(
+      pset.get<artdaq::Fragment::timestamp_t>("time_step_ms", 199)
+      );
+    
+  TLOG(TLVL_INFO) << "Preparing to send " << num_requests << " requests separated by " << std::chrono::milliseconds(time_step).count()  << " ms";
+  for (int ii = 0; ii < num_requests; ++ii) {
+    request_time += time_step;
+    std::this_thread::sleep_until (request_time);
+    sender.AddRequest(seq, request_time.time_since_epoch().count());
+    TLOG(TLVL_DEBUG) << "Sending request for Sequence ID " << seq << ", timestamp " << request_time.time_since_epoch().count();
     sender.SendRequest();
 
     if (receiver) {
@@ -59,14 +64,13 @@ int main(int argc, char* argv[]) {
           recvd = true;
         }
         else {
-          usleep(10000);
+          usleep(100);
         }
       }
-    }
+    } 
 
     seq += seq_scale;
-    ts += ts_scale;
-  }
+  } 
 
-  return rc;
+  return 0;
 }
