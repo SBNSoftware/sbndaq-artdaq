8f51ebf9695a4249cfb90e28fbba3d3264be634d
William Badgett <badgett@sbnd-daq00.fnal.gov>
2021-05-26 15:33:47 -0500

    templates for DAPHNE BR

---
+++ sbndaq-artdaq/Generators/SBND/DAPHNEReader.hh
@@ -0,0 +1,31 @@
+//
+// sbndaq-artdaq/Generators/SBND/DAPHNEReader.hh
+//
+
+#include "artdaq-core/Data/Fragment.hh"
+#include "artdaq/Generators/CommandableFragmentGenerator.hh"
+
+namespace sbndaq 
+{
+  class DAPHNEReader : public artdaq::CommandableFragmentGenerator 
+  {
+    public:
+      DAPHNEReader();
+      // "initialize" transition
+      explicit DAPHNEReader(fhicl::ParameterSet const& ps);
+      // "shutdown" transition
+      virtual ~DAPHNEReader();
+
+    protected:
+
+     // "start" transition
+     void start() override;
+ 
+     // "stop" transition
+     void stop() override;
+     bool getNext_(artdaq::FragmentPtrs& output) override;
+     void stopNoMutex() override {}
+
+     void setupDAPHNE(fhicl::ParameterSet const& ps);
+  };
+}
---
+++ sbndaq-artdaq/Generators/SBND/DAPHNEReader_generator.cc
@@ -0,0 +1,64 @@
+//
+//  sbndaq-artdaq/Generators/SBND/DAPHNEReader_generator.cc
+//
+//  Configuring board reader for the DAPHNE SiPM Readout
+//
+
+#include "DAPHNEReader.hh"
+#include "artdaq/Generators/GeneratorMacros.hh"
+#include "cetlib_except/exception.h"
+
+#include <sstream>
+#include <vector>
+#include <memory>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+
+namespace sbndaq 
+{
+
+// "initialize" transition
+DAPHNEReader::DAPHNEReader(fhicl::ParameterSet const& ps): CommandableFragmentGenerator(ps) 
+{
+  const std::string identification = "DAPHNEReader";
+  TLOG_INFO(identification) << "DAPHNEReader constructor" << TLOG_ENDL;
+  setupDAPHNE(ps);
+}
+
+void DAPHNEReader::setupDAPHNE(fhicl::ParameterSet const& ps) 
+{
+
+  const std::string identification = "DAPHNEReader::setupDAPHNE";
+  TLOG_INFO(identification) << "Starting setupDAPHNE " << TLOG_ENDL;
+}
+  
+
+// "shutdown" transition
+DAPHNEReader::~DAPHNEReader() 
+{
+
+}
+
+// "start" transition
+void DAPHNEReader::start() 
+{
+
+}
+
+// "stop" transition
+void DAPHNEReader::stop() 
+{
+
+}
+
+// Called by BoardReaderMain in a loop between "start" and "stop"
+bool DAPHNEReader::getNext_(artdaq::FragmentPtrs& /*frags*/) 
+{
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  return (! should_stop()); // returning false before should_stop makes all other BRs stop
+}
+
+} // namespace
+
+DEFINE_ARTDAQ_COMMANDABLE_GENERATOR(sbndaq::DAPHNEReader)
