127dfa9240656b56a47776657bf9cc05bcaa7009
Gennadiy <lukhanin@fnal.gov>
2021-09-16 22:05:49 -0500

    Update build scripts.

--- for-jenkins/publishscisoft.sh
+++ for-jenkins/publishscisoft.sh
@@ -16,17 +16,9 @@ function publishscisoft(){
 
  ./copyFromJenkins \
 -m SLF7 \
--q s106-e19 \
--q s108-e19 \
--q s109-e19 \
 -q s110-e19 \
--q s111-e19 \
 -q s112-e19 \
--q s106-e20 \
--q s108-e20 \
--q s109-e20 \
 -q s110-e20 \
--q s111-e20 \
 -q s112-e20 \
  sbndaq_artdaq-build
 
