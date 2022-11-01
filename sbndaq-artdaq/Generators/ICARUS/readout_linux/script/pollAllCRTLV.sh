55aee4318061df5e3f17acde5ff5678fc18648a3
rhowell42 <39918054+rhowell42@users.noreply.github.com>
2022-11-01 16:00:56 -0400

    Delete sbndaq-artdaq/Generators/ICARUS/readout_linux directory

--- sbndaq-artdaq/Generators/ICARUS/readout_linux/script/pollAllCRTLV.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Brief: Print out the status of all CRT low voltage channels
-#Usage: pollAllCRTLV.sh
-#Author: Andrew Olivier aolivier@ur.rochester.edu
-
-echo '#Top power supply:'
-pollCRTLV.exp np04-ps-crt-01.cern.ch
-echo '#Bottom power supply:'
-pollCRTLV.exp np04-ps-crt-02.cern.ch
--- sbndaq-artdaq/Generators/ICARUS/readout_linux/script/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-SCRIPT=`realpath ${BASH_SOURCE}`
-PATH_TO_SCRIPT=`dirname $SCRIPT`
-export PERL5LIB=$PATH_TO_SCRIPT/../macro:$PERL5LIB
-export PATH=$PATH_TO_SCRIPT:$PATH_TO_SCRIPT/../macro:$PATH
