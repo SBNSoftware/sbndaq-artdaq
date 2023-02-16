c436511c1f929b3c361df8e4d6ef1d0a1f21f7b2
Gennadiy <lukhanin@fnal.gov>
2023-02-16 17:45:08 -0600

    removed all vestigial backend code to prepare for PR

--- sbndaq-artdaq/Generators/ICARUS/BottomInterface/ICARUS_DAQ/ROOTntuple/ProcessData.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -l
-/usr/bin/perl /home/nfs/icarus/DAQ_DevAreas/DAQ_12Dec2022_rhowell/srcs/sbndaq_artdaq/sbndaq-artdaq/Generators/ICARUS/BottomInterface/ICARUS_DAQ/ROOTntuple/Wrapper.pl $1
