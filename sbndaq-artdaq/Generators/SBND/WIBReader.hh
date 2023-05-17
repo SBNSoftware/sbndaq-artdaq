8aaedadba20725c438e482dbc556ec953e2b8210
sbnd <sbnd@fnal.gov>
2023-05-17 11:59:19 -0500

    Added WIB fake data function.

--- sbndaq-artdaq/Generators/SBND/WIBReader.hh
+++ sbndaq-artdaq/Generators/SBND/WIBReader.hh
@@ -30,6 +30,7 @@ namespace sbndaq
 
      void setupWIB(fhicl::ParameterSet const& WIB_config);
      void setupFEMBFakeData(size_t iFEMB, fhicl::ParameterSet const& FEMB_config,bool continueOnFEMBRegReadError);
+     void setupWIBFakeData(int datamode); // This function is developed by looking into Modified_WIB_fake_data.py at "/home/nfs/sbnd/BNL_CE/SBND_CE"
      void setupFEMB(size_t iFEMB, fhicl::ParameterSet const& FEMB_configure);
      std::unique_ptr<WIB> wib;
   };
