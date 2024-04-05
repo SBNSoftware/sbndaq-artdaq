75cd72b127eb60e6c9b7b57e1b12d708c725c592
Gennadiy Lukhanin <glukhanin@solitonica.com>
2024-04-05 12:47:02 -0500

    Update build scripts

--- for-jenkins/build-sbndaq_artdaq.sh
+++ for-jenkins/build-sbndaq_artdaq.sh
@@ -48,10 +48,10 @@ IFS=':' read -r -a quals <<< "$qual_set"
 
 for onequal in "${quals[@]}"; do
   case ${onequal} in
-    e19|e20|e26|c[0-9])
+    e19|e20|e26|e28|c[0-9])
       basequal=${onequal}
       ;;
-    s1[0-2][0-9]*)
+    s1[0-9][0-9]*)
       squal=${onequal}
       ;;
     py2|py3)
